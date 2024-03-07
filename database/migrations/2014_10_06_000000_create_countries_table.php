20a7c621547ce21415a25ac3afb092bec197a103
millerjeison <millerjeison@gmail.com>
2024-03-07 10:45:39 -0500

    Location nullable

--- database/migrations/2014_10_06_000000_create_countries_table.php
+++ database/migrations/2014_10_06_000000_create_countries_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,7 +13,7 @@ return new class extends Migration {
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('iso_alpha_2', 2)->nullable(); // Campo para el código ISO 3166-1 alpha-2
 
             $table->timestamps();
--- database/migrations/2014_10_07_000000_create_states_table.php
+++ database/migrations/2014_10_07_000000_create_states_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -14,12 +15,11 @@ return new class extends Migration {
             $table->id();
             $table->unsignedBigInteger('country_id');
 
-            $table->string('name');
+            $table->string('name')->nullable();;
             $table->timestamps();
 
             $table->foreign('country_id')->references('id')->on('countries');
         });
-
     }
 
     /**
