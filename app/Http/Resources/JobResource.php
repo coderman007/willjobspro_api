d2c1a663d517350d80ff10bc3318f9f55b456599
Coderman007 <coderman1980@gmail.com>
2024-04-16 00:00:52 -0500

    Ajustes 3

--- app/Http/Resources/JobResource.php
+++ app/Http/Resources/JobResource.php
@@ -4,25 +4,19 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @method getAttribute(string $string)
+ */
 class JobResource extends JsonResource
 {
     public function toArray($request): array
     {
-        return [
+        $data = [
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'posted_date' => $this->posted_date,
             'deadline' => $this->deadline,
-            'location' => [
-                'country' => [
-                    'name' => $this->country->name,
-                    'dial_code' => $this->country->dial_code,
-                    'iso_alpha_2' => $this->country->iso_alpha_2],
-                'state' => $this->state->name,
-                'city' => $this->city->name,
-                'zip_code' => $this->zipCode->code,
-            ],
             'salary' => $this->salary,
             'contact_email' => $this->contact_email,
             'contact_phone' => $this->contact_phone,
@@ -32,8 +26,24 @@ class JobResource extends JsonResource
             'languages' => $this->getAttribute('languages'),
             'education_levels' => $this->getAttribute('educationLevels'),
             'skills' => $this->getAttribute('skills'),
-
         ];
+
+        // Verificar si la ubicación está presente
+        if ($this->country && $this->state && $this->city && $this->zipCode) {
+            $data['location'] = [
+                'country' => [
+                    'name' => $this->country->name,
+                    'dial_code' => $this->country->dial_code,
+                    'iso_alpha_2' => $this->country->iso_alpha_2
+                ],
+                'state' => $this->state->name,
+                'city' => $this->city->name,
+                'zip_code' => $this->zipCode->code,
+            ];
+        } else {
+            $data['location'] = null; // Ubicación no proporcionada
+        }
+
+        return $data;
     }
 }
-
