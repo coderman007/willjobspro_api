0cdace8d056af3077a239d3bbbc0c74e929d552c
Coderman007 <coderman1980@gmail.com>
2024-04-17 09:56:00 -0500

    Ajustes 1

--- app/Http/Resources/ApplicationResource.php
+++ app/Http/Resources/ApplicationResource.php
@@ -17,36 +17,34 @@ class ApplicationResource extends JsonResource
         $serverPath = 'https://coderman.pixela2.com.co/public/storage/';
 
         return [
-
             'candidate' => [
                 'id' => $this->candidate_id,
-                'gender' => $this->candidate->gender,
-                'date_of_birth' => $this->candidate->date_of_birth,
-                'country' => $this->candidate->user->country->name,
-                'expected_salary' => $this->candidate->expected_salary,
+                'gender' => $this->candidate->gender ?? null,
+                'date_of_birth' => $this->candidate->date_of_birth ?? null,
+                'country' => $this->candidate->user->country->name ?? null,
+                'expected_salary' => $this->candidate->expected_salary ?? null,
                 'cv' => $this->candidate->cv ? $serverPath . $this->candidate->cv : null,
                 'profile_photo' => $this->candidate->photo ? $serverPath . $this->candidate->photo : null,
-                'cover_letter' => $this->cover_letter,
-
+                'cover_letter' => $this->cover_letter ?? null,
             ],
 
             'job' => [
                 'id' => $this->job_id,
-                'title' => $this->job->title,
-                'salary' => $this->job->salary,
+                'title' => $this->job->title ?? null,
+                'salary' => $this->job->salary ?? null,
             ],
 
             'application' => [
                 'application_id' => $this->id,
                 'application_date' => $this->application_date,
-                'rejection_date' => $this->rejection_date,
+                'rejection_date' => $this->rejection_date ?? null,
                 'status' => $this->status,
             ],
 
             'company' => [
                 'id' => $this->job->company->id,
-                'name' => $this->job->company->user->name,
-                'logo' => $serverPath . $this->job->company->logo,
+                'name' => $this->job->company->user->name ?? null,
+                'logo' => $serverPath . ($this->job->company->logo ?? null),
             ],
         ];
     }
