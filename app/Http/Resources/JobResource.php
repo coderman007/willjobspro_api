b905dadda68dfc91ddba8b88bd8c8e46cfa1ce02
Coderman007 <coderman1980@gmail.com>
2024-05-24 11:59:13 -0500

    Optimización de filtros

--- app/Http/Resources/JobResource.php
+++ app/Http/Resources/JobResource.php
@@ -20,24 +20,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property mixed $video
  * @property mixed $jobCategory
  * @property mixed $applications
- * @property mixed $location
  */
 class JobResource extends JsonResource
 {
     public function toArray($request): array
     {
         $serverPath = 'https://coderman.pixela2.com.co/public/storage/';
-
-        $jobLocation = $this->location;
-        $companyLocation = $this->company->location;
-
-        if (!$jobLocation && $companyLocation) {
-            $locationString = $companyLocation->city . ', ' . $companyLocation->state . ', ' . $companyLocation->country;
-        } elseif (!$jobLocation && !$companyLocation) {
-            $locationString = 'Company does not have a location specified.';
-        } else {
-            $locationString = $jobLocation->city . ', ' . $jobLocation->state . ', ' . $jobLocation->country;
-        }
 
         return [
             'id' => $this->id,
@@ -57,9 +45,9 @@ class JobResource extends JsonResource
                 'name' => $this->company->user->name,
                 'company_name' => $this->company->company_name,
                 'address' => $this->company->user->address ?? null,
+                'location' => $this->company->user->country ?? "No company country defined",
                 'logo' => $this->company->logo ? $serverPath . $this->company->logo : null,
                 'banner' => $this->company->banner ? $serverPath . $this->company->banner : null,
-                'location' => $companyLocation ? $companyLocation->city . ', ' . $companyLocation->state . ', ' . $companyLocation->country : null,
             ],
             'benefits' => $this->getAttribute('benefits') ?? null,
             'job_category' => $this->jobCategory->name,
@@ -68,7 +56,6 @@ class JobResource extends JsonResource
             'education_levels' => $this->getAttribute('educationLevels') ?? null,
             'skills' => $this->getAttribute('skills') ?? null,
             'total_applications' => $this->applications->count(),
-            'location' => $locationString,
         ];
     }
 
